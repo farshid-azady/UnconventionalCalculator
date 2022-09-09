9dd3d1b56c8aae77f8d26481cfbb0ddd16140a7b
farshid-azady <jsx.asynchronouse@gmail.com>
2022-09-09 19:44:03 +0430

    along Developement of the calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,11 @@
+let defaultResult = 0;
+let currentResult = defaultResult;
 
+// Addition Calculation
+function add() {
+	currentResult= currentResult + userInput.value;
+	
+    outputResult(currentResult, 'CalculationDescriptor');
+}
+
+addBtn.addEventListener("click", add);

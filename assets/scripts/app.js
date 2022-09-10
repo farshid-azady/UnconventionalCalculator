b68c3c861fb07b3ec682aaff2a67c3c4877ae265
farshid-azady <jsx.asynchronouse@gmail.com>
2022-09-10 19:00:41 +0430

    add new function for EntryLog

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,27 @@
 let defaultResult = 0;
 let currentResult = defaultResult;
-let logEntries =[]
+let logEntries = [];
 
 function getUserNumberInput() {
 	return parseInt(userInput.value);
+}
+function writeToLog(
+	operationIdentifier,
+	prevResult,
+	operationNumber,
+	resultOperation
+) {
+	const logEntry = {
+		operation: operationIdentifier,
+		prevResult: prevResult,
+		number: operationNumber,
+		result: resultOperation,
+	};
+	// To Have userinput Log in console or Local Storage Furthermore.
+	logEntries.push(logEntry); //The Objects Pushed in Array And Classified As Arrays.
+	console.log(logEntry.operation);
+	console.log(logEntries);
+	console.dir("------------");
 }
 function createAndWriteOutput(Operator, initialresult, userinput) {
 	const DescriptionCalculationResult = `${initialresult} ${Operator} ${userinput}`;
@@ -16,35 +34,29 @@ function add() {
 	const initialresult = currentResult;
 	currentResult += enteredNumber;
 	createAndWriteOutput("+", initialresult, enteredNumber);
-    // To Have Full Details Of Data Entry in Log.
-    const logEntry={
-        operation:'ADD',
-        prevResult:initialresult,
-        number:enteredNumber,
-        result:currentResult
-    }
-    // To Have userinput Log in console or Local Storage Furthermore.
-     logEntries.push(logEntry) //The Objects Pushed in Array And Classified As Arrays.
-    console.log(logEntries)
-    console.dir('------------')
+	// To Have Full Details Of Data Entry in Log.
+	writeToLog("ADD", enteredNumber, initialresult, currentResult);
 }
 function subtract() {
 	const enteredNumber = getUserNumberInput();
 	const initialresult = currentResult;
-	currentResult -=enteredNumber;
+	currentResult -= enteredNumber;
 	createAndWriteOutput("-", initialresult, enteredNumber);
+    writeToLog("SUBTRACT", enteredNumber, initialresult, currentResult);
 }
 function divide() {
 	const enteredNumber = getUserNumberInput();
 	const initialresult = currentResult;
-	currentResult /=enteredNumber;
+	currentResult /= enteredNumber;
 	createAndWriteOutput("/", initialresult, enteredNumber);
+    writeToLog("DIVIDE", enteredNumber, initialresult, currentResult);
 }
 function multiply() {
 	const enteredNumber = getUserNumberInput();
 	const initialresult = currentResult;
-	currentResult *=enteredNumber;
+	currentResult *= enteredNumber;
 	createAndWriteOutput("*", initialresult, enteredNumber);
+    writeToLog("MULTIPLY", enteredNumber, initialresult, currentResult);
 }
 
 addBtn.addEventListener("click", add);
